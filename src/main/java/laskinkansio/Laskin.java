4ac96945d3991ec2da4996b43595117b45e574d4
Alessa Pentinmikko <118171088+alessaem@users.noreply.github.com>
2025-11-13 14:33:47 +0200

    initial commit

---
+++ src/main/java/laskinkansio/Laskin.java
@@ -0,0 +1,62 @@
+package laskinkansio;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class Laskin {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Laskin.class.getName());
+        Scanner sc = new Scanner(System.in);
+        logger.info("Enter the first number:");
+        double num1 = sc.nextDouble();
+        logger.info("Enter the second number:");
+        double num2 = sc.nextDouble();
+
+        logger.info("Select calculation: 1:sum, 2:subtract, 3:multiply, 4:divide ");
+        int calculation = sc.nextInt();
+
+        double result;
+        switch (calculation) {
+            case 1:
+                result = addValues(num1, num2);
+                break;
+            case 2:
+                result = subtract(num1, num2);
+                break;
+            case 3:
+                result = multiply(num1, num2);
+                break;
+            case 4:
+                if (num2 == 0) {
+                    logger.info("Cannot divide by zero");
+                    sc.close();
+                    return;
+                }
+                result = divide(num1, num2);
+                break;
+            default:
+                logger.warning("Unknown operation selected");
+                sc.close();
+                return;
+        }
+
+        logger.info(()->"The result is: " + result);
+
+    }
+
+    public static double addValues(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static double multiply(double num1, double num2){
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2){
+        return num1 / num2;
+    }
+
+    public static double subtract(double num1, double num2){
+        return num1-num2;
+    }
+}
---
+++ src/test/java/laskinkansio/LaskinTest.java
@@ -0,0 +1,33 @@
+package laskinkansio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LaskinTest {
+
+    @Test
+    void addValues(){
+        assertEquals(6, Laskin.addValues(3,3));
+        assertEquals(25, Laskin.addValues(20,5));
+    }
+
+    @Test
+    void subValues(){
+        assertEquals(6, Laskin.subtract(9,3));
+        assertEquals(-10, Laskin.subtract(20,30));
+    }
+
+    @Test
+    void multiplyValues(){
+        assertEquals(30, Laskin.multiply(6,5));
+        assertEquals(600, Laskin.multiply(20,30));
+    }
+
+    @Test
+    void divideValues(){
+        assertEquals(4, Laskin.divide(20,5));
+        assertEquals(6, Laskin.divide(30,5));
+    }
+
+}
